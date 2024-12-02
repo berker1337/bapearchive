feab0da980cda32ddd4fc3290ec0000366c3ac3e
berker13 <vojta.janota8@gmail.com>
2024-12-02 16:41:15 +0100

    16:41

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ const magazines = [
   { id: 4, title: 'Autumn / Winter 2012', image: '/assets/magazine4.png', pdf: '/assets/magazine4.pdf' },
   { id: 5, title: 'Summer 2012', image: '/assets/magazine5.png', pdf: '/assets/magazine5.pdf' },
   { id: 6, title: 'Spring 2012', image: '/assets/magazine6.png', pdf: '/assets/magazine6.pdf' },
+  { id: 7, title: 'Winter 2011', image: '/assets/magazine7.png', pdf: '/assets/magazine7.pdf' },
+  { id: 8, title: 'Autumn 2011', image: '/assets/magazine8.png', pdf: '/assets/magazine8.pdf' },
+  { id: 9, title: 'Summer 2011', image: '/assets/magazine9.png', pdf: '/assets/magazine9.pdf' },
+  { id: 10, title: 'Spring 2011', image: '/assets/magazine10.png', pdf: '/assets/magazine10.pdf' },
+  { id: 11, title: 'Winter 2010', image: '/assets/magazine11.png', pdf: '/assets/magazine11.pdf' },
+  { id: 12, title: 'Autumn 2010', image: '/assets/magazine12.png', pdf: '/assets/magazine12.pdf' },
+  { id: 13, title: 'Summer 2010', image: '/assets/magazine13.png', pdf: '/assets/magazine13.pdf' },
+  { id: 14, title: 'Spring 2010', image: '/assets/magazine14.png', pdf: '/assets/magazine14.pdf' },
 ];
 
 const App = () => {
